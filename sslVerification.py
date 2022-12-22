174427e7cbbb815234406c31838b73914d448a4c
Djakaridja Ouedraogo <tup09204@temple.edu>
2022-12-22 00:20:10 -0500

    update json response

--- sslVerification.py
+++ sslVerification.py
@@ -23,3 +23,40 @@ def check_ssl_expiration(event, context):
             'headers': { 'Content-Type': 'application/json' },
             'body': json.dumps({'error': str(e)})
         }
+        
+#define time
+    now = datetime.utcnow()  # current time
+    expiration_date = datetime.strptime(expiration_date, '%b %d %H:%M:%S %Y %Z') # expiration date
+    days_until_expiration = (expiration_date - now).days  # remaining date til expiration
+    is_valid = now < expiration_daten # if the expiration date is greater that the current date, the certifate is valid
+ 
+  # check expiration condition
+    if not is_valid:    # if the certificate is note valid...
+        sns = boto3.client('sns')
+        try:
+            sns.publish(TopicArn=sns_topic_arn, Message=f"SSL certificate for {domain} has expired") # send notifaction
+        except Exception as e:  # if an error occurs
+            
+            return {
+                'statusCode': 500,   # return internal server error
+                'headers': { 'Content-Type': 'application/json' },
+                'body': json.dumps({'error': str(e)})   # return the JSON version of the body
+            }
+
+    # create response
+    response = {
+        'domain_name': domain,
+        'days_until_expiration': days_until_expiration,
+        'is_valid': is_valid
+    }
+ #return response in JSON
+    return {
+        'statusCode': 200,      #indicate successful response
+        'headers': { 'Content-Type': 'application/json' },
+       'body': json.dumps(response)
+       }
+
+# In this function will call check_ssl_expiration with the event and context parsed.
+def lambda_handler(event, context):
+    response = check_ssl_expiration(event, context)
+    return response
